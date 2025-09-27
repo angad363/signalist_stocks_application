c3c847c349c9ca71aca286e33a92c7f2f415602f
Angad Anil Gosain <angadgosain@gmail.com>
2025-09-27 13:50:01 -0700

    Implemented Header component

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+const page = () => {
+  return (
+    <div className="flex min-h-screen home-wrapper">
+        Home
+    </div>
+  )
+}
+
+export default page
